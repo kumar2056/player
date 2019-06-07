e511bb93dab4808bf318372eb2993a8ab33e4dd0
kumar2056 <42170484+kumar2056@users.noreply.github.com>
2019-06-07 14:23:42 +0530

    Create roman.cpp

---
+++ roman.cpp
@@ -0,0 +1,92 @@
+//
+#include <iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    char a[1000];
+    cin>>a;
+    if(strcmp(a,"I")==0)
+    {
+        cout<<1;
+    }
+    else if(strcmp(a,"II")==0)
+    {
+        cout<<2;
+    }
+    else if(strcmp(a,"III")==0)
+    {
+        cout<<3;
+    }
+    else if(strcmp(a,"IV")==0)
+    {
+        cout<<4;
+    }
+    else if(strcmp(a,"V")==0)
+    {
+        cout<<5;
+    }
+    else if(strcmp(a,"VI")==0)
+    {
+        cout<<6;
+    }
+    else if(strcmp(a,"VII")==0)
+    {
+        cout<<7;
+    }
+    else if(strcmp(a,"VIII")==0)
+    {
+        cout<<8;
+    }
+    else if(strcmp(a,"IX")==0)
+    {
+        cout<<9;
+    }
+    else if(strcmp(a,"X")==0)
+    {
+        cout<<10;
+    }
+    else if(strcmp(a,"XI")==0)
+    {
+        cout<<11;
+    }
+    else if(strcmp(a,"XII")==0)
+    {
+        cout<<12;
+    }
+    else if(strcmp(a,"XIII")==0)
+    {
+        cout<<13;
+    }
+    else if(strcmp(a,"XIV")==0)
+    {
+        cout<<14;
+    }
+    else if(strcmp(a,"XV")==0)
+    {
+        cout<<15;
+    }
+    else if(strcmp(a,"XVI")==0)
+    {
+        cout<<16;
+    }
+    else if(strcmp(a,"XVII")==0)
+    {
+        cout<<17;
+    }
+    else if(strcmp(a,"XVIII")==0)
+    {
+        cout<<18;
+    }
+    else if(strcmp(a,"XIX")==0)
+    {
+        cout<<19;
+    }
+    else if(strcmp(a,"XX")==0)
+    {
+        cout<<20;
+    }
+return 0;
+}
