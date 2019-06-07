144a1fa44531ba3e9aa30082081f8be10b3bece6
kumar2056 <42170484+kumar2056@users.noreply.github.com>
2019-06-07 12:03:01 +0530

    Create cyclic.cpp

---
+++ cyclic.cpp
@@ -0,0 +1,29 @@
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[10000],b,c,d,v;
+    cin>>b>>c;
+    for(int i=0;i<b;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<c;i++)
+    {
+        
+        d=b-1;
+        v=a[d];
+        while(d!=0)
+        {
+            a[d]=a[d-1];
+            d=d-1;
+        }
+        a[0]=v;
+    }
+    for(int i=0;i<b;i++)
+    {
+        cout<<a[i]<<" ";
+    }
